ca8fd6acee5bdea6dfc0348217a33fea02f088fe
a_kiyanova <anastasiya.kiianova@gmail.com>
2023-07-02 16:55:55 +0000

    Added koa-router and process /home

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
 
 const port = 3000;
 const app = new Koa();
+const router = new KoaRouter();
 
 app.use(json())
+// app.use(async (ctx) => ( ctx.body = {Text : "Hello World"} ));
 
-app.use(async (ctx) => {
-        ctx.body = {Text : "Hello World"};
-})
+router.get('/home', ctx => ( ctx.body = {Text : "Welcome to koa server"} ))
+
+app.use(router.routes()).use( router.allowedMethods() );
 
 app.listen(port, () => console.log('Server Running on port: 3000.'));
