39dd5c5ddbb5b550ca21f563d4d8ce3f9e3084db
a_kiyanova <anastasiya.kiianova@gmail.com>
2023-07-02 16:46:50 +0000

    Changed to json response

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This chatbot is a simple API that sends back instructions according to some key 
 To start:
 ```git clone git@github.com:anastasiyakiianova/chatbot.git
 docker build -t koa:latest .
-docker run -it --name koa -dp 3000:3000 koa:latest```
+docker run -it --name koa -dp 3000:3000 koa:latest
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const Koa = require("koa");
+const Koa = require('koa');
+const json = require('koa-json');
+
+const port = 3000;
 const app = new Koa();
 
+app.use(json())
+
 app.use(async (ctx) => {
-        ctx.body = "Hello World";
+        ctx.body = {Text : "Hello World"};
 })
 
-app.listen(3000);
+app.listen(port, () => console.log('Server Running on port: 3000.'));
